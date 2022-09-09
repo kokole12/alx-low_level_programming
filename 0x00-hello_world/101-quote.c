a06785be3d3d8ad938f35587aa02d42a9182825e
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2022-09-09 03:49:37 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	printf("s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }

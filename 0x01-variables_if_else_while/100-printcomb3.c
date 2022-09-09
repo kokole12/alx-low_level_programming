3b0d35e85c934278e4bab326ab50c0445bbd156f
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2022-09-10 02:59:12 +0300

    comos

--- 0x01-variables_if_else_while/100-printcomb3.c
+++ 0x01-variables_if_else_while/100-printcomb3.c
@@ -6,3 +6,25 @@
  * Return: always returns zero
  *
  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 10; i <= 19; i++)
+	{
+		for (j = 10; j <= 19; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 19 ||  j != 19)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}

565e0c880dc1d07e3f7a68f30ded475c79a4d7d0
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2022-09-09 03:27:07 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc $CFILE -o "Programming is like building a multilingual puzzle
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+  puts("\""Programming is like building a multilingual puzzle");
+  return (0);
+}

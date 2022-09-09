03305c6a750555da3a46d2824b8a16c563280bfd
kokole12 <kokoleismail886@gmail.com>
2022-09-09 15:57:25 -0700

    aphabetes using a loop

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlid.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * RETURN:always returns 0
+ *
+ */
+int main(void)
+{
+	int n;
+	int lastval;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastval = n % 2;
+	if (lastval > 5)
+	{
+		printf("last digit %d is %d and is greater than 5\n", n, lastval);
+	} else if (lastval == 0)
+	{
+		pritnf("last digit of %d is %d and is 0\n", n, lastval);
+	} else
+	{
+		printf("last digit of %d is %d is is less than 6\n", n, lastval);
+	}
+	return (0);
+}

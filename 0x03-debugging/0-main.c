d05ab60ad8e5ea22e353186271d5e5ec6bb486f0
kokole12 <kokoleismail886@gmail.com>
2022-09-15 18:19:07 +0300

    postive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * 
+ * Return: always results to 0
+*/
+int main(void)
+{
+    int val;
+    val = 0;
+
+    positive_or_negative(val);
+    return (0);
+} 

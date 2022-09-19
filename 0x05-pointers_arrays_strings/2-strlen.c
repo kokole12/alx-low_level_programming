0fc5b9a817a7043e53294933ace94cce15301779
kokole12 <kokoleismail886@gmail.com>
2022-09-19 14:01:35 +0300

    function to get the length of the string from a character pointer as array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * _strlen - return the length of the string
+ *
+ * @s: is out test value to pass as an array of string to form the string and loop to find the length
+ * 
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{}
+return (i);
+}

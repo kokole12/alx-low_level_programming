111b31fc76a947016cf8422056f56648162624d9
kokole12 <kokoleismail886@gmail.com>
2022-09-21 20:10:32 +0300

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concats two strings
+ *
+ * @dest: dsetination array to cat
+ * @src: source array to cat
+ * @n: tracking value
+ *
+ * Return:  returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i;
+    int j;
+
+    for (i = 0; dest[i] != '\0'; i++);
+    for (j = 0; src[j] != '\0' && j < n; j++)
+    {
+        dest[i + j] = src[j];
+    }
+    dest[i + j] = '\0';
+    return dest;
+}

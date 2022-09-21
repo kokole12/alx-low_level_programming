9b255fcb7065a3c802caf361b2917595a8206cf6
kokole12 <kokoleismail886@gmail.com>
2022-09-21 12:47:56 +0300

    worked om advanced tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,31 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
- * _atoi - converts string to integer
- * @s: string to convert
- *
- * Return: returns integer value
+ * _atoi - print reverse characters.
+ * @s: Array
+ * Return: Always 0.
  */
+
 int _atoi(char *s)
 {
-int i;
-int j;
-int n;
-int x;
+int i = 0, sign = 1;
+unsigned int num = 0;
 
-i = n = 0;
-x = 1;
-while ((s[j] < '0' || s[i] > '9') && (s[i]!='\0'))
+while (s[i] != '\0')
 {
-if (s[i] == '-')
+if ((s[i] < '0' || s[i] > '9') && (s[i] == '-'))
 {
-x *= -1;
+sign = sign * -1;
+}
+else if ((s[i] >= '0' && s[i] <= '9'))
+{
+num = num * 10 + (s[i] - '0');
+if ((s[i + 1] < '0' || s[i + 1] > '9'))
+{
+break;
+}
 }
 i++;
 }
-j = i;
-while ((s[j] >= '0') && (s[j] <= '9'))
-{
-n = (n * 10) + x * ((s[j] - '0'));
-j++;
-}
-return (n);
+return (num * sign);
 }

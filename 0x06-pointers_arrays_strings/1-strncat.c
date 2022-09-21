f9cc0d374fef1586b0f261b46b9a146d95890cde
kokole12 <kokoleismail886@gmail.com>
2022-09-21 20:11:26 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    int j;
+int i;
+int j;
 
-    for (i = 0; dest[i] != '\0'; i++);
-    for (j = 0; src[j] != '\0' && j < n; j++)
-    {
-        dest[i + j] = src[j];
-    }
-    dest[i + j] = '\0';
-    return dest;
+for (i = 0; dest[i] != '\0'; i++);
+for (j = 0; src[j] != '\0' && j < n; j++)
+{
+dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
+return dest;
 }

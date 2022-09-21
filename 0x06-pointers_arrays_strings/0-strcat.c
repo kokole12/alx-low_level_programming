5137a2d8a8e3ec0f20bbd79e91e2927ea03219ae
kokole12 <kokoleismail886@gmail.com>
2022-09-21 19:44:40 +0300

    concatination

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,24 @@
 
 /**
  *
+ * *_strcat - concats two strings
+ * @dest: destination string
+ * @src: source string
  *
- *
- *
- *
+ * Return: returns success dest
  *
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	for (j = 0; src[j] != '\0'; j++)
-	       	dest[i+j] = src[j];
-       	dest[i+j] = '\0';
-       	return dest;
+for (i = 0; dest[i] != '\0'; i++){
+;
+}
+for (j = 0; src[j] != '\0'; j++){
+dest[i+j] = src[j];
+}
+dest[i+j] = '\0';
+return dest;
 }

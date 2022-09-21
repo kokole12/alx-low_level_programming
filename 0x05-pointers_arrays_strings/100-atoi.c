2040690e8ed9b73db5a82be322464cfb3d50e9ca
kokole12 <kokoleismail886@gmail.com>
2022-09-21 12:32:47 +0300

    worked om advanced tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,5 +9,22 @@
  */
 int _atoi(char *s)
 {
-return (0);
+int i, j, n, x;
+
+i = n = 0;
+x = 1;
+while ((s[j] < '0' || s[i] > '9') && (s[i]!='\0'))
+{
+if (s[i] == '-')
+{
+x *= -1;
+i++;
+}
+j = i;
+while ((s[j] >= '0') && (s[j] <= '9'))
+{
+n = (n * 10) + x * ((s[j] - '0'));
+j++;
+}
+return (n);
 }

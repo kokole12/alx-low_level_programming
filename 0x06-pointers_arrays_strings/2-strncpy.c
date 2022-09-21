3e9476a3b2ad58d1b560e783bbf93c90b46e8659
kokole12 <kokoleismail886@gmail.com>
2022-09-21 21:16:34 +0300

    worked on the errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
- * 
- * 
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    if (dest == NULL) {
-        return NULL;
-    }
- 
-    char* ptr = dest;
- 
-    while (*src && n--)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
- 
-    *dest = '\0';
- 
-    return ptr;
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }

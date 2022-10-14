aa6147029f5216cac796ba9595cdb200f3a17d3e
kokole12 <kokoleismail886@gmail.com>
2022-10-14 15:33:18 +0300

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,91 +1,71 @@
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "variadic_functions.h"
-/**
- * print_c - prints char
- * @a: list to give
- * Return: always 0
- */
-int print_c(va_list a)
-{
-printf("%c", va_arg(a, int));
-return (0);
-}
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-int print_i(va_list a)
-{
-printf("%d", va_arg(a, int));
-return (0);
-}
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-int print_f(va_list a)
-{
-printf("%f", va_arg(a, double));
-return (0);
-}
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-int print_s(va_list a)
-{
-char *s;
+#include <stddef.h>
 
-s = va_arg(a, char *);
-if (s == NULL)
-{
-printf("(nil)");
-return (0);
-}
-printf("%s", s);
-return (0);
-}
 /**
- * print_all - prints all
- * @format: format string that says arg types
+ * _strlen - string's size
  *
+ * @format: string parameter
+ *
+ * Return: unsigned integer
+ */
+unsigned int _strlen(const char * const format)
+{
+unsigned int size = 0;
+
+while (format[size])
+{
+size++;
+}
+
+return (size);
+}
+
+/**
+ * print_all - prints anything
+ *
+ * @format: variable's format to display
  */
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *sep = "";
-char *sep2 = ", ";
-va_list anyArgs;
-printer ops[] = {
-{"c", print_c},
-{"i", print_i},
-{"s", print_s},
-{"f", print_f},
-{NULL, NULL}
-};
+va_list list;
+unsigned int i = 0, validableCharacter = 0;
+char *s;
 
-va_start(anyArgs, format);
-i = 0;
-while (format != NULL && format[i])
+va_start(list, format);
+
+while (format && format[i])
 {
-j = 0;
-while (ops[j].f != NULL)
+validableCharacter = 0;
+switch (format[i])
 {
-if (format[i] == *(ops[j].c))
-{
-printf("%s", sep);
-ops[j].f(anyArgs);
+case 'c':
+printf("%c", va_arg(list, int));
+validableCharacter = 1;
+break;
+case 'i':
+printf("%i", va_arg(list, int));
+validableCharacter = 1;
+break;
+case 'f':
+printf("%f", va_arg(list, double));
+validableCharacter = 1;
+break;
+case 's':
+s = va_arg(list, char *);
+if (s == NULL)
+s = "(nil)";
+printf("%s", s);
+validableCharacter = 1;
+break;
 }
-j++;
-}
-sep = sep2;
+
+if (i < _strlen(format) - 1 && validableCharacter)
+printf(", ");
 i++;
 }
+
 printf("\n");
-va_end(anyArgs);
+
+va_end(list);
 }

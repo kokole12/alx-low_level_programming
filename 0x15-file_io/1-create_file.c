0fda4f38243696325db5edc5cc0fc8f91881f6e7
kokole12 <kokoleismail886@gmail.com>
2022-11-01 15:10:32 +0300

    fixed a betty error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,30 @@
 #include "main.h"
 /**
- * create_file - creates a file with rw------- permissions
- * @filename: name of the file, if NULL, return -1
- * @text_content: contents of the file. If NULL, create an empty file
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ * @filename: filename to grab
+ * @text_content: content to add
+ * Return: int value
  */
 int create_file(const char *filename, char *text_content)
 {
-int new_file;
-int len;
-int wr_stat;
+int fd, len = 0;
+long int wrote;
 
 if (filename == NULL)
 return (-1);
-new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-if (new_file == -1)
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
 return (-1);
-if (text_content == NULL)
+if (text_content != NULL)
 {
-close(new_file);
+
+while (text_content[len])
+len++;
+wrote = write(fd, text_content, len);
+if (wrote == -1)
+return (-1);
+}
+if (close(fd) == -1)
+return (-1);
 return (1);
-}
-for (len = 0; text_content[len]; len++);
-
-wr_stat = write(new_file, text_content, len);
-
-if (close(new_file) == -1)
-{
-return (-1);
-}
-return (wr_stat == -1 ? -1 : 1);
-
 }

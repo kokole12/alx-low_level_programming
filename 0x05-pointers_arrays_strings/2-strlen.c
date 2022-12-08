cb854f9cfd2f354e4eb00fe5bc612e4fabd1337a
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2022-12-08 19:07:12 +0300

    new clean code

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,13 @@
  * @s: is out test value to pass as an array
  * Return: returns the length
  */
-int _strlen(char *s)
-{
-int i;
+int _strlen(char *s) {
+  int i;
 
-for (i = 0; s[i] != '\0'; i++)
-{}
-return (i);
+  i = 0;
+
+  while (s[i] != '\0') {
+    i++;
+  }
+  return i;
 }

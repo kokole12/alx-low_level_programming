87565073b710c2d21fa8fe4c393e4277b5f6c193
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2022-12-08 19:00:12 +0300

    better code

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,11 +7,9 @@
  * @b: are the comparison integers
  *
  */
-void swap_int(int *a, int *b)
-{
-int x = *a;
-int y = *b;
-
-*a = y;
-*b = x;
+void swap_int(int *a, int *b) {
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }

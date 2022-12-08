3d3d51637b4dae520debf661147599f495caaa27
kokole12 <kokoleismail886@gmail.com>
2022-12-08 18:41:58 +0300

    debugginb corrected

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - entry point
- * 
- * Return: always results to 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-    int val;
-    val = 0;
+	int i;
 
-    positive_or_negative(val);
-    return (0);
-} 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
